d291e2afeee58bff335b7b287149e1b2319d7393
Iolanta Averyanova <iolantazhiznevskaya@gmail.com>
2023-02-11 04:44:12 +0300

    added responsive navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,57 +1,123 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import SwitchModeButton from "./SwitchModeButton";
 
-const NavBar: React.FC = () => {
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
+
+const NavBar: React.FC = (props: Props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
   const navigate = useNavigate();
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography
-            onClick={() => navigate("")}
+          onClick={() => navigate("")}
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1 }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            Home page
+            MUI
           </Typography>
-
-          <Button color="inherit" onClick={() => navigate("auth")}>
-            Login
-          </Button>
-          <Button color="inherit" onClick={() => navigate("personal")}>
-            Personal Page
-          </Button>
-          <Button color="inherit" onClick={() => navigate("collectionpage")}>
-            My collections
-          </Button>
-          <Button color="inherit" onClick={() => navigate("")}>
-            Logout
-          </Button>
-          <SwitchModeButton />
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Button color="inherit" onClick={() => navigate("auth")}>
+              Login
+            </Button>
+            <Button color="inherit" onClick={() => navigate("personal")}>
+              Personal Page
+            </Button>
+            <Button color="inherit" onClick={() => navigate("collectionpage")}>
+              My collections
+            </Button>
+            <Button color="inherit" onClick={() => navigate("")}>
+              Logout
+            </Button>
+            <SwitchModeButton />
+          </Box>
         </Toolbar>
       </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      {/* <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box> */}
     </Box>
   );
 };
 
 export default NavBar;
-
-
